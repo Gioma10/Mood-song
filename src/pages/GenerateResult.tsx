1d5da248f95dc04777a8cb793298717bb0bae8cb
NicolasBrazzo <nicolasbrazzo8@gmail.com>
2025-03-24 18:36:08 +0100

    first Historical update

--- src/pages/GenerateResult.tsx
+++ src/pages/GenerateResult.tsx
@@ -16,14 +16,14 @@ interface Props {
 const GenerateResult: React.FC<Props> = ({ answer }) => {
     const [songsByEmotion, setSongsByEmotion] = useState<Playlist[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
-
+    const [selectedPlaylists, setSelectedPlaylists] = useState<{ [key: string]: boolean }>({});
 
     // Effetto per caricare le canzoni dal localStorage al primo caricamento della pagina
     useEffect(() => {
-        const storedSongs = localStorage.getItem('songsByEmotion');
-        if (storedSongs) {
-            setSongsByEmotion(JSON.parse(storedSongs)); // Imposta le canzoni dallo storage
-            setTimeout(()=>setLoading(false), 8000); // Aggiungi un flag di caricamento falso perché sono già disponibili
+            const storedSongs = localStorage.getItem('songsByEmotion');
+            if (storedSongs) {
+                setSongsByEmotion(JSON.parse(storedSongs)); // Imposta le canzoni dallo storage
+                setTimeout(() => setLoading(false), 8000); // Aggiungi un flag di caricamento falso perché sono già disponibili
         }
     }, []);
 
@@ -41,7 +41,7 @@ const GenerateResult: React.FC<Props> = ({ answer }) => {
                 const token = await getSpotifyToken(emotionsForSpotify);
                 if (!token) {
                     console.error("Token Spotify non ottenuto!");
-                    setTimeout(()=>setLoading(false), 8000);
+                    setTimeout(() => setLoading(false), 8000);
                     return;
                 }
 
@@ -51,7 +51,7 @@ const GenerateResult: React.FC<Props> = ({ answer }) => {
 
                 // Utilizziamo il prompt generato per cercare le canzoni
                 const offset = Math.floor(Math.random() * 1000);  // Varia l'offset per "saltare" brani casuali
-                const limit = Math.min(Number(answer.songsQuantity), 100 ); // Limita il numero di canzoni richieste a 50
+                const limit = Math.min(Number(answer.songsQuantity), 100); // Limita il numero di canzoni richieste a 50
 
                 const searchUrl = `https://api.spotify.com/v1/search?q=${encodeURIComponent(searchQuery)}&type=playlist&limit=${limit}&offset=${offset}`;
 
@@ -75,7 +75,7 @@ const GenerateResult: React.FC<Props> = ({ answer }) => {
             } catch (error) {
                 console.error("Errore nel recupero delle canzoni:", error);
             } finally {
-                setTimeout(()=>setLoading(false), 8000);
+                setTimeout(() => setLoading(false), 8000);
             }
         };
 
@@ -85,49 +85,79 @@ const GenerateResult: React.FC<Props> = ({ answer }) => {
         }
     }, [answer, songsByEmotion.length]); // La dipendenza di songsByEmotion.length assicura che non venga eseguito più di una volta
 
+    // Funzione per cambiare lo stato della playlist
+    const handleSelectPlaylist = (playlist: Playlist) => {
+        setSelectedPlaylists(prevState => {
+            const newState = {
+            ...prevState,
+            [playlist.id]: !prevState[playlist.id], // Inverte lo stato di selezione
+            };
+
+            // Se la playlist è selezionata, aggiungila al localStorage
+            if(newState[playlist.id]) {
+                const storedSongs = JSON.parse(localStorage.getItem('songsByEmotion') || '[]');
+                const updateSongs = [...storedSongs, playlist];
+                localStorage.setItem('songByEmotion', JSON.stringify(updateSongs));
+            } else {
+                // Se la playlist è deselezionata, rimuovila dal localStorage
+                const storedSongs = JSON.parse(localStorage.getItem('songByEmotion') || '[]');
+                const updateSongs = storedSongs.filter((song: Playlist) => song.id !== playlist.id);
+                localStorage.setItem('songByEmotion', JSON.stringify(updateSongs));
+            }
+
+            return newState;
+        });
+    };
+
     return (
         <>
             {loading ? (
                 <Loading />
-            ) 
-            : 
-            (
-            <div className="h-full w-full mt-30">
-                <h2 className="mt-10 flex text-3xl font-bold mb-6 text-center items-center justify-center">
-                    Canzoni in base alle emozioni: {answer.emotions.length === 1 ? answer.emotions : answer.emotions.join(", ")}
-                </h2>
-                <div className="pl-10 pr-10">
-                    <div className="space-y-8 flex justify-center items-center flex-wrap">
-                        {songsByEmotion.length > 0 ? (
-                            songsByEmotion.map((playlist) => (
-                                <div key={playlist.id} className="flex p-4 rounded-lg shadow-md bg-[#1b1b1b] w-full">
-                                    <div className="flex-1">
-                                        <h4 className="font-semibold text-lg">{playlist.name}</h4>
-                                        <a
-                                            href={playlist.external_urls.spotify}
-                                            target="_blank"
-                                            rel="noopener noreferrer"
-                                            className="text-purple-500 hover:text-purple-600 block mt-2 hover:underline"
-                                        >
-                                            Apri la playlist su Spotify
-                                        </a>
-                                    </div>
-                                    <div className="ml-4">
-                                        <img
-                                            src={playlist.images[0]?.url}
-                                            alt={playlist.name}
-                                            className="w-32 h-32 object-cover rounded-lg"
-                                        />
-                                    </div>
-                                </div>
-                            ))
-                        ) : (
-                            <p className="text-gray-400">Nessuna canzone trovata per queste emozioni.</p>
-                        )}
+            )
+                :
+                (
+                    <div className="h-full w-full mt-30">
+                        <h2 className="mt-10 flex text-3xl font-bold mb-6 text-center items-center justify-center">
+                            Canzoni in base alle emozioni: {answer.emotions.length === 1 ? answer.emotions : answer.emotions.join(", ")}
+                        </h2>
+                        <div className="pl-10 pr-10">
+                            <div className="space-y-8 flex justify-center items-center flex-wrap">
+                                {songsByEmotion.length > 0 ? (
+                                    songsByEmotion.map((playlist) => (
+                                        <div key={playlist.id} className="flex p-4 rounded-lg shadow-md bg-[#1b1b1b] w-full">
+                                            <div className="flex-1">
+                                                <h4 className="font-semibold text-lg">{playlist.name}</h4>
+                                                <a
+                                                    href={playlist.external_urls.spotify}
+                                                    target="_blank"
+                                                    rel="noopener noreferrer"
+                                                    className="text-purple-500 hover:text-purple-600 block mt-2 hover:underline"
+                                                >
+                                                    Apri la playlist su Spotify
+                                                </a>
+                                                <div 
+                                                className="text-4xl" 
+                                                onClick={() => handleSelectPlaylist(playlist)}>
+                                                    
+                                                    {selectedPlaylists[playlist.id] ? "Selezionato" : "+"}
+                                                </div>
+                                            </div>
+                                            <div className="ml-4">
+                                                <img
+                                                    src={playlist.images[0]?.url}
+                                                    alt={playlist.name}
+                                                    className="w-32 h-32 object-cover rounded-lg"
+                                                />
+                                            </div>
+                                        </div>
+                                    ))
+                                ) : (
+                                    <p className="text-gray-400">Nessuna canzone trovata per queste emozioni.</p>
+                                )}
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            )}
+                )}
         </>
     );
 };
--- src/pages/Historical.tsx
+++ src/pages/Historical.tsx
@@ -1,56 +1,78 @@
-import Button from "../components/Button"
+import { useEffect, useState } from "react";
+import Button from "../components/Button";
 import Emotion from "../components/Emotions";
 
 const Historical = () => {
+    // Definiamo il tipo per una playlist
+    interface Playlist {
+        name: string;
+        images: { url: string }[];
+    }
+
+    // Stato per gestire le playlist
+    const [playlists, setPlaylists] = useState<Playlist[]>([]);
+    console.log(playlists);
+
+    // Recuperiamo i dati dal localStorage all'avvio
+    useEffect(() => {
+        const storedData = localStorage.getItem("songsByEmotion");
+        if (storedData) {
+            try {
+                setPlaylists(JSON.parse(storedData));
+            } catch (error) {
+                console.error("Errore nel parsing dei dati:", error);
+                setPlaylists([]); // Se c'è un errore, usiamo un array vuoto
+            }
+        }
+    }, []);
 
     return (
         <div className="w-full h-[100%] pt-30">
             <h1 className="w-[200px] text-center m-auto text-5xl mb-10">Historical</h1>
-            <div className="w-[90%]  bg-slate-300 h-80 m-auto text-black rounded-3xl mb-36">
+
+            <div className="w-[90%] bg-slate-300 h-100 m-auto text-black rounded-3xl mb-36">
                 <h2 className="text-3xl text-center mt-10 pt-10">Ultime Canzoni</h2>
-                <div className=" flex justify-around items-center mt-10">
-                    <div className=" flex gap-3">
-                        <p>immagine</p>
-                        <p>titolo canzone - Cantante</p>
-                    </div>
-                    <div className=" flex gap-3">
-                        <p>immagine</p>
-                        <p>titolo canzone - Cantante</p>
-                    </div>
-                    <div className=" flex gap-3">
-                        <p>immagine</p>
-                        <p>titolo canzone - Cantante</p>
+                <div className="flex justify-around items-center mt-10">
+                    <div className="flex w-[100%] text-center items-center justify-center">
+                        {playlists.length > 0 ? (
+                            <ul className="flex justify-around w-[100%]">
+                                {playlists.slice(-3).map((playlist, index) => {
+                                    // Controllo per evitare errori se non ci sono immagini
+                                    const imageUrl = playlist.images.length > 0 ? playlist.images[0].url : "URL_IMMAGINE_DEFAULT";
+
+                                    return (
+                                        <li key={index} className="flex flex-col items-center">
+                                            <p>{playlist.name}</p>
+                                            <img
+                                                src={imageUrl}
+                                                alt={playlist.name}
+                                                className="w-24 h-24 object-cover mt-2"
+                                            />
+                                        </li>
+                                    );
+                                })}
+                            </ul>
+                        ) : (
+                            <p className="text-center mt-5">Nessuna playlist trovata</p>
+                        )}
                     </div>
                 </div>
-                <div className=" p-0 mt-10 text-center">
-                    <Button text="Visualizza tutto" path=""></Button>
+
+
+                <div className="p-0 mt-10 text-center">
+                    <Button text="Visualizza tutto" path="" />
                 </div>
             </div>
 
-
-            <div className="w-[90%]  bg-slate-300 h-100 m-auto text-black rounded-3xl mb-56">
+            <div className="w-[90%] bg-slate-300 h-100 m-auto text-black rounded-3xl mb-56">
                 <h2 className="text-3xl text-center mt-10 pt-10">Canzoni per Emozioni</h2>
-                
+
                 <div className="flex flex-wrap mt-8">
-                    <Emotion bgColor="green" name="felicita" timeListen={10}/>
-                    <Emotion bgColor="green" name="felicita" timeListen={10}/>
-                    <Emotion bgColor="green" name="felicita" timeListen={10}/>
-                    <Emotion bgColor="green" name="felicita" timeListen={10}/>
-                    <Emotion bgColor="green" name="felicita" timeListen={10}/>
-                    <Emotion bgColor="green" name="felicita" timeListen={10}/>
-                    <Emotion bgColor="green" name="felicita" timeListen={10}/>
-                    <Emotion bgColor="green" name="felicita" timeListen={10}/>
-                    <Emotion bgColor="green" name="felicita" timeListen={10}/>
-                    <Emotion bgColor="green" name="felicita" timeListen={10}/>
-                    <Emotion bgColor="green" name="felicita" timeListen={10}/>
-                    <Emotion bgColor="green" name="felicita" timeListen={10}/>
+                    
                 </div>
-                
             </div>
-
-
         </div>
-    )
-}
+    );
+};
 
-export default Historical; 
+export default Historical;
